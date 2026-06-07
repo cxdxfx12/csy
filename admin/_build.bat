6d5709f9d4f5d1534dad1e7cd23d8973eb529d61
admin <cxdxfx@github.com>
2026-06-07 23:20:43 +0800

    修复官网图片丢失问题：添加post-build图片复制步骤，提交静态图片到public/admin/assets/images

--- admin/_build.bat
+++ admin/_build.bat
@@ -3,5 +3,11 @@ cd /d e:\ds\admin
 echo Building admin frontend...
 call "C:\Program Files\nodejs\npm.cmd" run build
 echo.
-echo Build complete! Check admin/dist/ for output.
+echo Copying static images...
+if not exist "..\public\admin\assets\images" mkdir "..\public\admin\assets\images"
+copy /Y "src\assets\images\welcome-girl.jpg" "..\public\admin\assets\images\" >nul
+copy /Y "src\assets\images\monkey-ico.png" "..\public\admin\assets\images\monkey-icon.png" >nul
+echo Static images copied.
+echo.
+echo Build complete!
 pause
